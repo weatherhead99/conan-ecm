ed35a8aa2aa5181a2ea7281f29f1859a3e0f9fb0
Dan Weatherill <plasteredparrot@gmail.com>
2018-09-12 04:03:29 +0100

    make it OS dependent, export files different on each platform

--- conanfile.py
+++ conanfile.py
@@ -7,6 +7,7 @@ class ECMConan(ConanFile):
     url = "https://github.com/weatherhead99/conan-ecm"
     description = "Extra modules and scripts for CMake."
     generators="cmake"
+    settings="os"
     sha256="edf6f7895c307afae893200c759f8bb4803650f61b39409f3fce6a9efd50746d"
     short_paths=True
 
